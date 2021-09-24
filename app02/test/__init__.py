9eae004fc7911b7d7d9cc9845ff861fb988e6c53
zxiaosi <2692159310@qq.com>
2021-09-24 20:30:17 +0800

    V2.3 测试两种初始化表数据的方法

---
+++ app02/test/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+# @Time : 2021/9/24 20:27
+# @Author : 小四先生
+# @desc : 测试文件
---
+++ app02/test/data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+# @Time : 2021/9/24 19:40
+# @Author : 小四先生
+# @desc : 数据
+from app02.models.user import User
+
+userData = [
+    User(user_id='4', full_name='小二', password='123'),
+    User(user_id='5', full_name='小三', password='123')
+]
+
+data = [
+    {'user_id': 6, 'full_name': '小刘', 'password': '123'},
+    {'user_id': 7, 'full_name': '小赵', 'password': '123'},
+]
